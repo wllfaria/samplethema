13ef6c8921d79bbfab3ad3ca7f8bc4947f7589cc
Willians Faria <dev.willians.faria@gmail.com>
2024-08-31 12:50:11 -0300

    message passing example

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+mod index;
+mod messages;
+
+use anathema::backend::tui::TuiBackend;
+use anathema::runtime::Runtime;
+use anathema::templates::Document;
+
+use index::Index;
+use messages::Messages;
+
+fn main() {
+    let doc = Document::new("@index");
+    let backend = TuiBackend::builder()
+        .enable_alt_screen()
+        .enable_raw_mode()
+        .enable_mouse()
+        .hide_cursor()
+        .finish()
+        .unwrap();
+
+    let mut runtime = Runtime::builder(doc, backend);
+
+    let recipient = runtime
+        .register_default::<Messages>("messages", "templates/messages.aml")
+        .expect("failed to register messages component");
+
+    runtime
+        .register_component("index", "templates/index.aml", Index::new(recipient), ())
+        .expect("failed to register index component");
+
+    runtime.finish().unwrap().run();
+}
